8bd2191d2e2eafd91f0b0bc4dcd2b75fa9f61242
nikan010 <9022105@student.zadkine.nl>
2023-11-17 13:47:01 +0100

    eeeeee

---
+++ opdracht.php
@@ -0,0 +1,29 @@
+<?php
+  //auteur: yvan  van dijk
+  //functie: uitleg datum functie
+
+  //installatie:
+  $datum = date("l d F Y");
+
+  //main
+
+  echo "datum is: $datum";
+
+  echo "<br>";
+  echo "vandaag is het de: " . date("z") . "e dag van het jaar";
+  echo "<br>";
+  echo date("l") . " is de " . date("w") . "e dag van de week";
+  echo "<br>";
+  echo "de maand " . date("F") . " heeft in totaal " . date("t") . " dagen";
+  echo "<br>";
+  echo "het jaar " . date("Y");
+  $t = date("L");
+  if ($t < "0") {
+echo " is geen schrikkeljaar";
+}
+
+  if ($t < "1") {
+echo " is een schrikkeljaar";
+}
+
+?>
