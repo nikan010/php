e0d2621def9397e39a74e4540e323fb9cfd4bfdf
nikan010 <9022105@student.zadkine.nl>
2023-11-23 09:44:56 +0100

    dddd

---
+++ opdracht2.php
@@ -0,0 +1,18 @@
+
+    <?php
+    // Definieer twee stringvariabelen
+    $string1 = "Dit is de eerste string.";
+    $string2 = "Dit is de tweede string.";
+
+    // Definieer twee numerieke variabelen
+    $nummer1 = 10;
+    $nummer2 = 20;
+
+    // Gebruik echo om de stringvariabelen op het scherm te tonen
+    echo "<p>$string1</p>";
+    echo "<p>$string2</p>";
+
+    // Gebruik print om de numerieke variabelen op het scherm te tonen
+    print("<p>$nummer1</p>");
+    print("<p>$nummer2</p>");
+    ?>
