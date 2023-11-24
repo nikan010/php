f12ba355c2ece2cbd6767a610f32094d1593b124
nikan010 <9022105@student.zadkine.nl>
2023-11-24 14:40:07 +0100

    ss

--- teamsopdracht.php
+++ teamsopdracht.php
@@ -7,13 +7,18 @@
     // temp > 18: kachel uit
 
     // Initialisatie
-    $kachel = 0;
+    $kachel = -19;
 
     if ($kachel >= -10 && $kachel <= 0) {
         echo "Hoge stand.";
     } elseif ($kachel > 0 && $kachel <= 18) {
         echo "Normale stand.";
-    } else {
+    } if ($kachel == 18) {
         echo "Kachel uit.";
+    } if ($kachel <= -18) {
+        echo "u gaat dood";
     }
+   
+    
+
 ?>
